4b90eb1804db904002914882b1b552d7f593aa3e
vidya <vidya.palliyalil@gmail.com>
2025-02-12 18:32:29 +0530

    creat react app using CRA structued the folder

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import './App.css';
+
+function App() {
+  return (
+    <>
+    <div style ={{ display: "flex", justifyContent: "center"}}>
+      {/* <h1>Hi</h1> */}
+      <h1>Hello World</h1>
+    </div>
+    <div style ={{ display: "flex", justifyContent: "center"}}>
+    <h1>Hi</h1>
+    {/* <h1>Hello World</h1> */}
+  </div>
+  <div className="App">
+      <h1>Hello World2</h1>
+    </div>
+  </>
+    
+  );
+}
+
+export default App;
